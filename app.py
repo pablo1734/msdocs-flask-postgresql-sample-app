f5dcfb572d2e0069d5fd17f6b1bd166f598e750e
pablo1734 <pablo17alonsoz@gmail.com>
2025-05-01 20:02:28 +0200

    añadido jsonify

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for, jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
